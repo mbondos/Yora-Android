d15ae0d469b100831f387f91fbfb41d36f043912
Maksymilian Bondos <m4vypl@gmail.com>
2017-02-15 20:24:43 +0100

    Animations

--- app/src/main/java/tk/mbondos/yora/activities/BaseActivity.java
+++ app/src/main/java/tk/mbondos/yora/activities/BaseActivity.java
@@ -2,10 +2,12 @@ package tk.mbondos.yora.activities;
 
 
 
+import android.animation.Animator;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import tk.mbondos.yora.R;
 import tk.mbondos.yora.infrastructure.YoraApplication;
@@ -31,14 +33,51 @@ public abstract class BaseActivity extends AppCompatActivity {
         if(toolbar != null)
             setSupportActionBar(toolbar);
     }
+    public void fadeOut(final FadeOutListener listener) {
+        View rootView = findViewById(android.R.id.content);
+        rootView.animate()
+                .alpha(0)
+                .setListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        listener.onFadeOutEnd();
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                })
+                .setDuration(300)
+                .start();
+    }
+
     protected void setNavDrawer(NavDrawer drawer) {
         this.navDrawer = drawer;
         this.navDrawer.create();
+
+        overridePendingTransition(0, 0);
+        View rootView = findViewById(android.R.id.content);
+        rootView.setAlpha(0);
+        rootView.animate().alpha(1).setDuration(450).start();
     }
     public Toolbar getToolbar() {
         return toolbar;
     }
     public YoraApplication getYoraApplication() {
         return application;
+    }
+    public interface FadeOutListener {
+        void onFadeOutEnd();
     }
 }
